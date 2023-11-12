064678cc1906e9881733f173229b3fc3a088f90c
humakarahan <149956628+humakarahan@users.noreply.github.com>
2023-11-12 13:00:50 +0300

    Add files via upload

---
+++ hafta5ödev.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int sayi;
+    cout<< "sayi giriniz:\n";
+    cin>> sayi;
+    if(sayi>10)
+    sayi=sayi-10;
+    else
+    sayi=sayi+10;
+
+    cout<<sayi;
+    return 0;
+
+}
